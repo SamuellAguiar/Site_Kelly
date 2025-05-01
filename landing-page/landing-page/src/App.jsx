ef8c798e9f76fda31f61166dce7543d76d0aa8bd
SamuellNote <samuellcarlosaguiar@gmail.com>
2025-05-01 11:37:56 -0300

    feat: adicionar melhorias no componente App.jsx
    - Ajustes realizados no componente App.jsx para melhorar a funcionalidade e a experiência do usuário.

--- landing-page/landing-page/src/App.jsx
+++ landing-page/landing-page/src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from './components/navbar/navbar';
-import Home from './components/home/home';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 import AboutUs from './components/about us/AboutUs';
-import Dentista1 from './components/dentista1/dentista1';
+import Dentista1 from './components/dentista1/Dentista1';
 import Dentista2 from './components/dentista2/Dentista2';
-import ContactForm from './components/contactForm/contactForm';
+import ContactForm from './components/contactForm/ContactForm';
 import Footer from './components/footer/Footer';
 import ScrollToTop from './components/scrollToTop/ScrollToTop';
 import './App.css';
