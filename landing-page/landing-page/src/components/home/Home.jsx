b6d0466d553773cd9284175ebcfc24a5bb13974a
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-05-20 22:05:07 -0300

    feat(home): atualizar estilos e componentes da página inicial
    - Ajusta o arquivo Home.css para melhorias visuais
    - Atualiza o componente Home.jsx com novas funcionalidades e correções

--- landing-page/landing-page/src/components/home/Home.jsx
+++ landing-page/landing-page/src/components/home/Home.jsx
@@ -4,8 +4,8 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home-wrapper" id="home">
-      <img src="/6.png" alt="Imagem 1" className="home-img" />
-      <img src="/DSC_1015-removebg-preview.png" alt="Imagem 2" className="home-img" />
+      <img src="/6.png" alt="Imagem 1" className="home-img left" />
+      <img src="/DSC_1015.jpg" alt="Imagem 2" className="home-img right" />
     </div>
   );
 }
