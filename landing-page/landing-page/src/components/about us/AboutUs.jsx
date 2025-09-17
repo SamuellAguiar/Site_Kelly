d96e6d57f7c94e3e66c0b2205e982c8abb142567
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2025-09-17 11:30:04 -0300

    feat(about-us): update AboutUs component
    Refactor and improve the AboutUs.jsx component for better readability and maintainability.

--- landing-page/landing-page/src/components/about us/AboutUs.jsx
+++ landing-page/landing-page/src/components/about us/AboutUs.jsx
@@ -6,7 +6,6 @@ const AboutUs = () => {
     <section className="sobre-mim-section" id="sobre-nos">
       <div className="sobre-mim-container">
         <div className="sobre-mim-image">
-          {/* Substitua este SVG por outro quando tiver o definitivo */}
           <img src="home.jpeg" alt="Imagem sobre o curso" className="aboutUs-img" />
         </div>
         <div className="sobre-mim-texto">
@@ -14,7 +13,6 @@ const AboutUs = () => {
           <p>
             <i>Resinas compostas em dentes anteriores por Kelly Aguiar e Laís Albergaria</i> visa preparar estudantes e profissionais da odontologia a desenvolverem habilidades com a resina composta, que permitirão elevar o nível técnico dos procedimentos! <br />
             Unindo teoria e prática, sempre embasadas em uma odontologia ética e baseada em evidências, buscamos que os alunos sejam capacitados a realizarem desde procedimentos mais básicos até procedimentos complexos de estratificação em dentes anteriores, com resultados o mais natural possível!
-
           </p>
         </div>
       </div>
